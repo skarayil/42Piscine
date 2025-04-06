1d676de7541d970731bc910fa6c424a1bc916425
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-06 22:55:12 +0300

    sude naz karayildirim

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -20,13 +20,13 @@ void	ft_is_negative(int n)
 		write(1, "P", 1);
 }
 
-
-int	main(void)
+/*
+int     main(void)
 {
-	ft_is_negative(42);
-	printf("\n");
-	ft_is_negative(0);
-	printf("\n");
-	ft_is_negative(-42);
-	printf("\n");
+        ft_is_negative(42);
+        write(1, "\n", 1);
+        ft_is_negative(0);
+        write(1, "\n", 1);
+        ft_is_negative(-42);
 }
+*/

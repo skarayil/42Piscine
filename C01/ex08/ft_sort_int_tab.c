ab3dac5271f2003ba264727612b47045970a8197
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-06 23:45:03 +0300

    sude naz karayildirim

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -13,32 +13,34 @@
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	change;
-	int	i;
+	int	index;
 	int	temp;
 
 	if (size <= 1)
 		return ;
 	change = 1;
-	i = 0;
+	index = 0;
 	while (change)
 	{
 		change = 0;
-		i = 0;
-		while (i < size - 1)
+		index = 0;
+		while (index < size - 1)
 		{
-			if (tab[i] > tab[i + 1])
+			if (tab[index] > tab[index + 1])
 			{
-				temp = tab[i];
-				tab[i] = tab[i + 1];
-				tab[i + 1] = temp;
+				temp = tab[index];
+				tab[index] = tab[index + 1];
+				tab[index + 1] = temp;
 				change = 1;
 			}
-			i++;
+			index++;
 		}
 	}
 }
 
-
+/*
+#include <stdio.h>
+	
 int	main(void)
 {
 	int	tab[10] = {5, 3, 2, 9, 4, 8, 0, 7, 1, 6};
@@ -63,3 +65,4 @@ int	main(void)
 		j++;
 	}
 }
+*/

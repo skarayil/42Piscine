cdd08c4e4e235e65da34b3592fdcb68dae294b65
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-06 23:23:51 +0300

    sude naz karayildirim

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -15,26 +15,29 @@ void	ft_ultimate_ft(int *********nbr)
 	*********nbr = 42;
 }
 
+/*
+#include <stdio.h>
 
 int	main(void)
 {
-	int	number = 1024;
-	int	*ptr1 = &number;
+	int	nbr = 1024;
+	int	*ptr1 = &nbr;
 	int	**ptr2 = &ptr1;
  	int	***ptr3 = &ptr2;
  	int	****ptr4 = &ptr3;
-	int *****ptr5 = &ptr4;
+	int	*****ptr5 = &ptr4;
  	int	******ptr6 = &ptr5;
  	int	*******ptr7 = &ptr6;
  	int	********ptr8 = &ptr7;
  	int	*********ptr9 = &ptr8;
 
-	printf("'Number' has the adress %p and the value %d\n", &number, number);
+	printf("'Number' has the adress %p and the value %d\n", &nbr, nbr);
 	printf("ptr1 points to same adress of 'Number'%p and has the same value %d\n", ptr1, *ptr1);
 	printf("ptr9 points to a different adress of 'Number'%p but the adresses are connected. It has the same value %d\n", ptr9, *********ptr9);
 	ft_ultimate_ft(ptr9);
 	printf("\n");
 	printf("After ft_ultimate_ft: \n");
 	printf("Pointer 'ptr9' remains pointing to adress %p, but now that has the value %d stored\n", ptr9, *********ptr9);
-	printf("'Number' remains pointing to adress %p, but now that has the value %d stored\n", &number, number);
+	printf("'Number' remains pointing to adress %p, but now that has the value %d stored\n", &nbr, nbr);
 }
+*/

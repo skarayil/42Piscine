9a54a6c804138a188c2a96cb34238887930c79dc
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-07 16:21:43 +0300

    sude naz karayildirim

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -12,14 +12,11 @@
 
 int	ft_str_is_lowercase(char *str)
 {
-	int	index;
-
-	index = 0;
-	while (str[index] != '\0')
+	while (*str != '\0')
 	{
-		if (!(str[index] >= 'a' && str[index] <= 'z'))
+		if (!(*str >= 'a' && *str <= 'z'))
 			return (0);
-		index++;
+		str++;
 	}
 	return (1);
 }

d2b36d38d6d93783e22f1a6f3f373bb8ad777b6a
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-07 16:18:08 +0300

    sude naz karayildirim

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -12,15 +12,11 @@
 
 int	ft_str_is_alpha(char *str)
 {
-	int	index;
-
-	index = 0;
-	while (str[index] != '\0')
+	while (*str != '\0')
 	{
-		if (!((str[index] >= 'a' && str[index] <= 'z')
-				|| (str[index] >= 'A' && str[index] <= 'Z')))
+		if (!((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z')))
 			return (0);
-		index++;
+		str++;
 	}
 	return (1);
 }

7863737d7f40f60a661140454413725204a88837
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-08 12:03:01 +0300

    sude naz karayildirim

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -12,23 +12,27 @@
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int	i;
+	unsigned int	index;
 
-	i = 0;
-	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	if (i == n)
+	index = 0;
+	while (index < n && s1[index] && s2[index] && s1[index] == s2[index])
+		index++;
+	if (index == n)
 		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 }
 
-
+/*
+#include <stdio.h>
+#include <string.h>
+	
 int	main()
 {
-	char s1[] = "word";
-	char s2[] = "Word";
-	int n = 2;
+	char s1[] = "42ecole";
+	char s2[] = "42Ecole";
+	int n = 3;
 
 	printf("Or: %d\n", strncmp(s1, s2, n));
 	printf("My: %d\n", ft_strncmp(s1, s2, n));
 }
+*/

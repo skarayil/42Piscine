922207dea1ba7156444f92494aa7da96268f3224
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-04-09 13:33:28 +0300

    sude naz karayildirim

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -34,7 +34,7 @@ int	ft_iterative_power(int nb, int power)
 	
 int	main(void)
 {
-	printf("Power of 2 ^ -1 = %d\n", ft_iterative_power(2, -1));
+	printf("Power of 2 ^ -1 = %d\n", ft_iterative_power(3, -1));
 	printf("Power of 0 ^ 0 = %d\n", ft_iterative_power(0, 0));
 	printf("Power of 3 ^ 1 = %d\n", ft_iterative_power(3, 1));
 	printf("Power of -3 ^ 3 = %d\n", ft_iterative_power(-3, 3));

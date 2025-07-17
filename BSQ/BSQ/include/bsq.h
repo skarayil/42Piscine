1b0f8e971276717cbbf21e4d5a5644cf090386ba
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2025-07-17 14:42:24 +0300

    sude naz karayildirim

--- BSQ/BSQ/include/bsq.h
+++ BSQ/BSQ/include/bsq.h
@@ -10,9 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// Starts a Header Guard
 #ifndef BSQ_H
 # define BSQ_H
 
+// Includes standard libraries
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/types.h>
@@ -21,6 +23,8 @@
 # include <stdbool.h>
 # include <stddef.h>
 
+// Defines a structure
+// Structures may contain variables that can be assigned after
 typedef struct		s_map
 {
 	int				rows;
@@ -34,10 +38,12 @@ typedef struct		s_map
 	int				square_col;
 }					t_map;
 
-t_map				*ft_create_map(char *file_name);
-t_map				*ft_map_error(void);
+// defines the prototypes of the functions that can be called by this header
 void				*ft_memcpy(void *dest, void *src, size_t num);
 void				*ft_realloc(void *ptr, size_t old_size, size_t new_size);
+int					ft_strcmp(char *s1, char *s2);
+int					ft_atoi(char *str);
+t_map				*ft_map_error(void);
 void				ft_free_map(t_map *map);
 void				ft_get_square(t_map *map);
 void				ft_process_map(t_map *map);
@@ -45,7 +51,7 @@ int					ft_get_line(t_map *map, int i, int file_name);
 int					ft_get_first_line(t_map *map, int file_name);
 int					ft_get_matrix(t_map *map, int file_name);
 int					ft_map_header(t_map *map, int file_name);
-int					ft_strcmp(char *s1, char *s2);
-int					ft_atoi(char *str);
+t_map				*ft_create_map(char *file_name);
 
+// Ends the Header Guard
 #endif
